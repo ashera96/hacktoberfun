d48bb3bcdbac2b2e86ea0014b6b1e656b729f42b
Ashera Silva <asherasilva100@gmail.com>
2018-10-21 21:36:53 +0530

    Python program to find if a given integer is even or odd
    Added OddEven.py python program to determine if a given input integer is odd or even

---
+++ even-odd/OddEven.py
@@ -0,0 +1,8 @@
+def odd_even(num):
+    if (num%2==0):
+        print("The number you entered is even");
+    else:
+        print("The number you entered is odd");
+
+n = int(input("Enter an integer : "))
+odd_even(n)
